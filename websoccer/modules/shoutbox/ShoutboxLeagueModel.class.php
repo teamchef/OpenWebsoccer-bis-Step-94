fd5874898aa96c1373e852745c00705dedf1ce52
Rolf Joseph / ErdemCan <rolfjoseph@eadress.de>
2015-05-12 19:36:04 +0200

    Shoutbox: neu gestaltet
    - aufgeteilt in einem Message & Eingabe-Bereich
    - der Message-Bereich wird automatisch refreshed
    
    Der Refresh der Shoutbox ist außerhalb des Livespiels limitiert, da zwei
    Refreshs, die über einen Button gesteuert werden pro Seite nicht möglich
    sind.
    
    Wann die Shoutbox aktualisiert wurde, wird ebenfalls angezeigt.

--- websoccer/modules/shoutbox/ShoutboxLeagueModel.class.php
+++ websoccer/modules/shoutbox/ShoutboxLeagueModel.class.php
@@ -3,19 +3,19 @@
 
   This file is part of OpenWebSoccer-Sim.
 
-  OpenWebSoccer-Sim is free software: you can redistribute it 
-  and/or modify it under the terms of the 
-  GNU Lesser General Public License 
+  OpenWebSoccer-Sim is free software: you can redistribute it
+  and/or modify it under the terms of the
+  GNU Lesser General Public License
   as published by the Free Software Foundation, either version 3 of
   the License, or any later version.
 
   OpenWebSoccer-Sim is distributed in the hope that it will be
   useful, but WITHOUT ANY WARRANTY; without even the implied
-  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
   See the GNU Lesser General Public License for more details.
 
-  You should have received a copy of the GNU Lesser General Public 
-  License along with OpenWebSoccer-Sim.  
+  You should have received a copy of the GNU Lesser General Public
+  License along with OpenWebSoccer-Sim.
   If not, see <http://www.gnu.org/licenses/>.
 
 ******************************************************/
@@ -28,13 +28,13 @@ class ShoutboxLeagueModel implements IModel {
 	private $_i18n;
 	private $_websoccer;
 	private $_leagueId;
-	
+
 	public function __construct($db, $i18n, $websoccer) {
 		$this->_db = $db;
 		$this->_i18n = $i18n;
 		$this->_websoccer = $websoccer;
 	}
-	
+
 	/**
 	 * (non-PHPdoc)
 	 * @see IModel::renderView()
@@ -43,14 +43,14 @@ class ShoutboxLeagueModel implements IModel {
 		$this->_leagueId = $this->_websoccer->getRequestParameter('id');
 		return ($this->_leagueId != NULL);
 	}
-	
+
 	/**
 	 * (non-PHPdoc)
 	 * @see IModel::getTemplateParameters()
 	 */
 	public function getTemplateParameters() {
 		$messages = array();
-		
+
 		// query latest shoutbox messages from the same competition
 		$tablePrefix = $this->_websoccer->getConfig('db_prefix');
 		$fromTable = $tablePrefix . '_shoutmessage AS MESSAGE';
@@ -60,9 +60,9 @@ class ShoutboxLeagueModel implements IModel {
 		$fromTable .= ' INNER JOIN ' . $tablePrefix . '_verein AS GUEST ON GUEST.id = M.gast_verein';
 		$fromTable .= ' INNER JOIN ' . $tablePrefix . '_saison AS SEASON ON (M.saison_id = SEASON.id)';
 		$fromTable .= ' INNER JOIN ' . $tablePrefix . '_liga AS L ON (L.id = SEASON.liga_id)';
-		
+
 		$whereCondition = 'L.id = %d ORDER BY MESSAGE.created_date DESC';
-		
+
 		$columns = array(
 				'MESSAGE.id' => 'message_id',
 				'MESSAGE.message' => 'message',
@@ -73,16 +73,16 @@ class ShoutboxLeagueModel implements IModel {
 				'GUEST.name' => 'guest_name',
 				'M.id' => 'match_id'
 		);
-		
-		$result = $this->_db->querySelect($columns, $fromTable, $whereCondition, $this->_leagueId, 20);
+
+		$result = $this->_db->querySelect($columns, $fromTable, $whereCondition, $this->_leagueId, 10);
 		while ($message = $result->fetch_array()) {
 			$messages[] = $message;
 		}
 		$result->free();
-		
+
 		return array("messages" => $messages, "hidesubmit" => TRUE);
 	}
-	
+
 }
 
 ?>
